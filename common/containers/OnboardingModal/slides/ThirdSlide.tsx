b74a89c357f6b79010be01b703629a9cedf00831
c-dega <loverly@protonmail.com>
2018-10-04 17:47:13 -0400

    removed stray bullet from onboarding slides

--- common/containers/OnboardingModal/slides/ThirdSlide.tsx
+++ common/containers/OnboardingModal/slides/ThirdSlide.tsx
@@ -28,11 +28,6 @@ export default function ThirdSlide() {
               </ul>
             </li>
             <li>
-              {/* {translate('ONBOARDING_TEXT_23', {
-                $link: 'https://etherscamdb.info/'
-              })} */}
-            </li>
-            <li>
               {translate('ONBOARDING_TEXT_24', {
                 $link: 'https://wallet.eximchain.com/download'
               })}

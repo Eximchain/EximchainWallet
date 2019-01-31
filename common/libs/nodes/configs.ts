e8ffcfb56ce8e78fb57697a219e2d9fa984a80ca
Andrew Koh <andrew@eximchain.com>
2019-01-31 10:53:04 -0500

    added configs for transaction executors

--- common/libs/nodes/configs.ts
+++ common/libs/nodes/configs.ts
@@ -8,10 +8,28 @@ export const makeNodeName = (network: string, name: string) => {
 export const NODE_CONFIGS: { [key in StaticNetworkIds]: RawNodeConfig[] } = {
   ETH: [
     {
-      name: makeNodeName('ETH', 'eximchain wallet'),
+      name: makeNodeName('ETH', 'localhost'),
       type: 'rpc',
       service: 'Localhost',
       url: 'http://localhost:22000'
+    },
+    {
+      name: makeNodeName('ETH', 'transactionexc1'),
+      type: 'rpc',
+      service: 'Europe',
+      url: 'http://ec2-3-80-165-200.compute-1.amazonaws.com:8080'
+    },
+    {
+      name: makeNodeName('ETH', 'transactionexc2'),
+      type: 'rpc',
+      service: 'Korea',
+      url: 'http://ec2-34-219-140-122.us-west-2.compute.amazonaws.com:8080'
+    },
+    {
+      name: makeNodeName('ETH', 'transactionexc3'),
+      type: 'rpc',
+      service: 'Singapore',
+      url: 'http://ec2-54-255-248-144.ap-southeast-1.compute.amazonaws.com:8080'
     }
   ]
 };

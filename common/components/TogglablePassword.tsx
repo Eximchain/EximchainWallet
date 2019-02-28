3a84a95e668a07cc19ada5a906d6eb74bba809fb
c-dega <loverly@protonmail.com>
2019-02-28 14:55:55 -0500

    visual updates to togglable password

--- common/components/TogglablePassword.tsx
+++ common/components/TogglablePassword.tsx
@@ -91,7 +91,7 @@ export default class TogglablePassword extends React.PureComponent<Props, State>
             name={name}
             disabled={disabled}
             type={isVisible ? 'text' : 'password'}
-            className={`${className} border-rad-right-0`}
+            className={`${className} border-rad-right-0 border-right-0`}
             placeholder={placeholder}
             onChange={onChange}
             onFocus={onFocus}

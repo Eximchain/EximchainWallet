86155e28dcb51e67a07c7efcbcdc2d85d1cb8feb
Andrew Koh <andrew@eximchain.com>
2019-03-04 17:10:01 -0500

    added state props, and added a transactionreset so as to not automatically set values from other contract call states

--- common/containers/Tabs/Governance/components/FreeContractCallScreen.tsx
+++ common/containers/Tabs/Governance/components/FreeContractCallScreen.tsx
@@ -69,7 +69,7 @@ interface ContractOption {
 
 type Props = StateProps & DispatchProps & OwnProps;
 
-export class FreeContractCallClass extends Component<Props> {
+export class FreeContractCallClass extends Component<Props, State> {
   public static defaultProps: Partial<Props> = {};
 
   public state: State = {
@@ -255,11 +255,6 @@ export class FreeContractCallClass extends Component<Props> {
     return selectedFunction!.contract.encodeInput(parsedInputs);
   }
   private handleBooleanDropdownChange = ({ value, name }: { value: boolean; name: string }) => {
-    if (name === '_election') {
-      this.setState({
-        promoDemoBool: value.toString()
-      });
-    }
     this.setState({
       inputs: {
         ...this.state.inputs,

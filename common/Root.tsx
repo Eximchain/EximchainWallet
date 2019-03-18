177979698772a260d158b3716123df0aa6a67f5c
c-dega <loverly@protonmail.com>
2019-03-18 16:57:45 -0400

    modal updates

--- common/Root.tsx
+++ common/Root.tsx
@@ -120,7 +120,7 @@ class RootClass extends Component<Props, State> {
             </React.Fragment>
           </Router>
         </Provider>
-        <div id="ModalContainer" />
+        <div className="ModalContainer" id="ModalContainer" />
       </React.Fragment>
     );
   }

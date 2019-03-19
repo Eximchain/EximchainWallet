ad5c321d3c3b3338ae51166f80746678107a19a8
Douglas Sanchez <douglas@eximchain.com>
2019-03-19 16:18:05 -0400

    Updated style of amount field.

--- common/components/SendEverything/SendEverything.tsx
+++ common/components/SendEverything/SendEverything.tsx
@@ -32,7 +32,7 @@ class SendEverythingClass extends Component<Props> {
             onClick={this.onSendEverything}
             aria-label={translateRaw('SEND_TRANSFERTOTAL')}
           >
-            <i className="SendEverything-icon fa fa-balance-scale" />
+            {translate('SEND_TRANSFERTOTAL')}
             <Tooltip>{translate('SEND_TRANSFERTOTAL')}</Tooltip>
           </button>
         )}

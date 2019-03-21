136a5767d50f66b7aa10c2e4e8c071e67e97982d
c-dega <loverly@protonmail.com>
2019-03-21 17:14:44 -0400

    new offline icon

--- common/containers/TabSection/OfflineTab.tsx
+++ common/containers/TabSection/OfflineTab.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import KeystoreIcon from 'assets/images/wallets/keystore.svg';
+import OfflineIcon from 'assets/images/offline.svg';
 
 import './OfflineTab.scss';
 
 const OfflineTab: React.SFC<{}> = () => (
   <section className="OfflineTab Tab-content swap-tab">
     <div className="Tab-content-pane">
-      <div className="OfflineTab-icon fa-stack fa-4x">
-        <i className="fa fa-wifi fa-stack-1x" />
-        <img src={KeystoreIcon} />
-        <i className="fa fa-ban fa-stack-2x" />
-      </div>
+      <img src={OfflineIcon} />
+
+      {
+        // <div className="OfflineTab-icon fa-stack fa-8x">
+        // <i className="fa fa-wifi fa-stack-1x" />
+        // <i className="fa fa-ban fa-stack-2x" />
+        // </div>
+      }
+
       <br />
       <br />
       <br />
